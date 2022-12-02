8ea03104cb1b918697eccfe97dd0924661a9267a
AndreFoulem <timmycasual@icloud.com>
2022-12-01 20:24:41 -0800

    Second ex:

--- binarytree.js
+++ binarytree.js
@@ -1,21 +1,8 @@
-function takeShower() {
-  return 'showering';
-}
+function countDown(num) {
+   while (num >= 0) {
+      console.log(num);
+      num--;
+      countDown(num);
+   }
 
-function eatBreakfast() {
-  let meal = cookFood();
-  return `Eating ${meal}`;
-}
-
-function cookFood() {
-  let items = ['Oatmeal', 'Eggs', 'Protein Shake'];
-  return items[Math.floor(Math.random() * items.length)];
-}
-
-function wakeUp() {
-  takeShower();
-  eatBreakfast();
-  console.log('Ok ready to go to work!');
-}
-
-wakeUp();
+countDown(4);
