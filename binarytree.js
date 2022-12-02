0969104974499207d2713dd2f97a62fbb96798e8
AndreFoulem <timmycasual@icloud.com>
2022-12-01 20:32:20 -0800

    Recursion

--- binarytree.js
+++ binarytree.js
@@ -1,8 +1,17 @@
-function countDown(num) {
-   while (num >= 0) {
-      console.log(num);
-      num--;
-      countDown(num);
-   }
+//base case
 
-countDown(4);
+function sumRanges(num) {
+  //STOP GAP - BASE CASE
+  if (num === 1) return 1;
+  //
+
+  return num + sumRanges(num - 1);
+}
+sumRanges(3);
+
+/* -----------  
+ await return 3 + sumRanges(2)
+            await return  2 + sumRanges(1);
+                           await return 1
+
+------------- */
