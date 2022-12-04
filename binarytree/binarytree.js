2cad3145091a902eca7feedd9dab87381a93b053
AndreFoulem <timmycasual@icloud.com>
2022-12-03 19:32:31 -0500

    Doc BST

--- binarytree/binarytree.js
+++ binarytree/binarytree.js
@@ -1,5 +1,17 @@
 //Binary search tree (sorted)
 
+//* -----------  Inserting A Node  ------------- //
+//& 1) Create a new node
+//& 2) Start at the root
+//&   A) If !Root  => node = root
+//&   B) If Root:
+//&      i) if node > root =>
+//&           a) if (!right node) => node = right node
+//&           b) if(right node) => move to it + repeat steps
+//&      ii) if node < root => check if (left node)
+//&           a) if (!left node) => node = left node
+//&           b) if (left node)=> move to it + repeat steps
+
 class BinarySearchTree {
   constructor() {
     this.root = null;
@@ -13,3 +25,11 @@ class Node {
     this.right = null;
   }
 }
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+
+tree.root.left.right(0);
