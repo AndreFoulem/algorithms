71686473f5a3bf394cb92aa2f177bb5a1d75d868
AndreFoulem <timmycasual@icloud.com>
2022-12-04 15:05:25 -0500

    Add first left node

--- binarytree/binarytree.js
+++ binarytree/binarytree.js
@@ -12,23 +12,33 @@
 //&           a) if (!left node) => node = left node
 //&           b) if (left node)=> move to it + repeat steps
 
-//?      TREE
-//?       10
-//?   5        13
-//? 2   7   11    16
-
 class BinarySearchTree {
   constructor() {
     this.root = null;
   }
   insert(value) {
     let newNode = new Node(value);
+    console.log('newnode', newNode);
     if (this.root === null) {
       this.root = newNode;
       return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          }
+        }
+      }
     }
   }
 }
+//?      TREE
+//?       10
+//?   5        13
+//? 2   7   11    16
 
 class Node {
   constructor(value) {
@@ -39,10 +49,12 @@ class Node {
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
+tree.insert(10);
+tree.insert(5);
+// tree.root = new Node(10);
 
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
 
 // tree.root.left.right(0);
 
