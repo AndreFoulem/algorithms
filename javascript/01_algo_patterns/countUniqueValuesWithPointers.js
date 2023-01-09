8cff5d3c7ba871ff59697573e99eff52432e0fe3
AndreFoulem <timmycasual@icloud.com>
2023-01-08 16:06:13 -0800

    Clean up comments

--- javascript/01_algo_patterns/countUniqueValuesWithPointers.js
+++ javascript/01_algo_patterns/countUniqueValuesWithPointers.js
@@ -1,8 +1,9 @@
+//! SORT UNIQUE W/ SWAPPY VALUES + 2 POINTERS
 //! Count unique values with pointers
 
 function countUniqueValues(arrayParam) {
-  var i = 0;
-  for (const j = 1; j < arrayParam.length; j++) {}
+  let i = 0;
+  for (j = 1; j < arrayParam.length; j++) {}
 }
 
 /**
@@ -10,6 +11,6 @@ function countUniqueValues(arrayParam) {
  i
 [1, 2, 3, 5, 6, 6, 8, 10, 10, 12];
     j
-    
+
 */
 countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);
