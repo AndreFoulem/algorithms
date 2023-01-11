ce231c0440ad14e956ac15be46689c98c7774411
AndreFoulem <timmycasual@icloud.com>
2023-01-10 20:35:55 -0800

    Complete sliding windows

--- javascript/01_algo_patterns/sliding_windows.js
+++ javascript/01_algo_patterns/sliding_windows.js
@@ -8,26 +8,56 @@
 
 //? Ex: maxSubarraySum([1,2,5,2,8,15],2)  => 10
 
-//& Naive solution
+//! O(N)
+function maxSubarraySum1(arr, num) {
+  const passedArgs = guards(arr, num);
+  if (passedArgs.isInvalid) return passedArgs.error;
 
+  let maxSum = 0;
+  let tempSum = 0;
+  for (i = 0; i < num; i++) {
+    //i/0   i<3  i++
+    //! sum = 1
+    //i/1   i<3  i++
+    //! sum = 1+2
+    //i/2   i<3  i++
+    //! sum = 3+5 = 7
+
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+  for (i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    //! 8 =8  - arr[0] + arr[3]
+    //! 8 =8  - arr[1] + arr[4] = 9
+
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+console.log(maxSubarraySum1([1, 2, 5, 2, 8, 15], 3));
+
+//! Naive solution - Quadratic
 function maxSubarraySum(arr, num) {
   const args = guards(arr, num);
   if (args.isInvalid) return args.error;
 
   let max = -Infinity;
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - num + 1; i++) {
     temp = 0;
     for (let j = 0; j < num; j++) {
       temp += arr[i + j];
+      console.log(temp);
     }
     if (temp > max) {
+      console.log(temp);
       max = temp;
     }
   }
   return max;
 }
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 15], 3));
 
 function guards(arr, num) {
   if (!Array.isArray(arr)) {
