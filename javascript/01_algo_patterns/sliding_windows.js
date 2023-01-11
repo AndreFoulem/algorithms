04cbabb0dcb411cb62cca63eec0d02f7562d663e
AndreFoulem <timmycasual@icloud.com>
2023-01-10 19:43:39 -0800

    Add sliding windows

---
+++ javascript/01_algo_patterns/sliding_windows.js
@@ -0,0 +1,9 @@
+//! Sliding Window: - Involves creating a 'window' which can either be an
+//!     array or number from 1 pos to another.
+//!   - Depending on condition, the window increases or closes(open new win)
+
+//? 1) Find longest string of unique characters "hellothere"
+//? 2) Write a fn which accepts arr of ints + 1 num called N.
+//?       Fn should calculate the max sum of N consecutive ele in arr.
+
+//? Ex: maxSubarraySum([1,2,5,2,8,15],2)  => 10
