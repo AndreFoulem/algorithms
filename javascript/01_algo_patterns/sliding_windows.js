782ee63cc2103a9379efe429e9f5ca3bc1fad9ac
AndreFoulem <timmycasual@icloud.com>
2023-01-10 20:14:07 -0800

    Add sliding windows naive

--- javascript/01_algo_patterns/sliding_windows.js
+++ javascript/01_algo_patterns/sliding_windows.js
@@ -7,3 +7,37 @@
 //?       Fn should calculate the max sum of N consecutive ele in arr.
 
 //? Ex: maxSubarraySum([1,2,5,2,8,15],2)  => 10
+
+//& Naive solution
+
+function maxSubarraySum(arr, num) {
+  const args = guards(arr, num);
+  if (args.isInvalid) return args.error;
+
+  let max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 15], 3));
+
+function guards(arr, num) {
+  if (!Array.isArray(arr)) {
+    return { isInvalid: true, error: 'arr is not an array' };
+  }
+  if (!(arr.length > 2)) {
+    return { isInvalid: true, error: 'arr is not an array' };
+  }
+  if (typeof num !== 'number') {
+    return { isInvalid: true, error: 'sum param is not a number' };
+  }
+  return false;
+}
