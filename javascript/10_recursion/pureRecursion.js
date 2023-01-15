6ef9818decb9e709336e0653d426b396b536d171
AndreFoulem <timmycasual@icloud.com>
2023-01-14 17:45:32 -0800

    Add pure recursive collectOddValues

--- javascript/10_recursion/pureRecursion.js
+++ javascript/10_recursion/pureRecursion.js
@@ -1,1 +1,20 @@
 //! Pure recursion
+
+function collectOddValues(arr) {
+  //
+  let newArr = []
+
+  if (arr.length === 0) {
+    return newArr
+  }
+
+  if (arr[0] % 2 !== 2) {
+    newArr.push(arr[0])
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)))
+
+  return newArr
+}
+
+collectOddValues([1, 2, 3, 4, 5])
