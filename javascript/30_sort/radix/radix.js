f5a2873bd6e1981b92f5217fbed4a2ee9fe2c1ff
AndreFoulem <timmycasual@icloud.com>
2023-01-23 15:35:01 -0800

    Add mostDigits

--- javascript/30_sort/radix/radix.js
+++ javascript/30_sort/radix/radix.js
@@ -1,12 +1,21 @@
 //! Radix sort
 
-function getDigit(num, i) {
-  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
-}
-
 function digitCount(num) {
   if (num === 0) return 1
   return Math.floor(Math.log10(Math.abs(num))) + 1
 }
 
-function mostDigits(num) {}
+//! 1: Loop to find largest digit for the radix loop iteration count
+function mostDigits(nums) {
+  let maxDigits = 0
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+  }
+  return maxDigits
+}
+
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
+}
+
+console.log(mostDigits([2, 333, 44, 66666666]))
