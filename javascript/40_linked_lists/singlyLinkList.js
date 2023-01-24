d9b78660c55489b24800c8b5b747b619eaa4f75a
AndreFoulem <timmycasual@icloud.com>
2023-01-24 14:41:36 -0800

    Add push

--- javascript/40_linked_lists/singlyLinkList.js
+++ javascript/40_linked_lists/singlyLinkList.js
@@ -6,3 +6,28 @@ class Node {
     this.next = null
   }
 }
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0
+    this.head = null
+    this.tail = null
+  }
+  push(value) {
+    let newNode = new Node(value)
+    // assign refs
+    if (this.head) {
+      this.tail.next = newNode
+      this.tail = newNode
+    } else {
+      this.head = newNode
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+}
+
+let list = new SinglyLinkedList()
+list.push('hello')
+console.log(list.push('hi'))
