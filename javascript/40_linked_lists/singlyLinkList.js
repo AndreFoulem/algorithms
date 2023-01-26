409dfbc0f5e0ef04c9228cba97bb561c053130c5
AndreFoulem <timmycasual@icloud.com>
2023-01-25 17:25:32 -0800

    Add pop to SinglinkList

--- javascript/40_linked_lists/singlyLinkList.js
+++ javascript/40_linked_lists/singlyLinkList.js
@@ -17,6 +17,7 @@ class SinglyLinkedList {
     let newNode = new Node(value)
     // assign refs
     if (this.head) {
+      //!-> tail holds ref to old node
       this.tail.next = newNode
       this.tail = newNode
     } else {
@@ -26,8 +27,36 @@ class SinglyLinkedList {
     this.length++
     return this
   }
+
+  pop() {
+    if (!this.head) {
+      return 'empty'
+    }
+    //! first thing is to traverse the list
+    let current = this.head
+    let newTail = current
+    while (current.next) {
+      newTail = current
+      current = current.next
+    }
+    this.tail = newTail
+    //!sever the last tail
+    this.tail.next = null
+
+    this.length--
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
+    }
+    return current
+    console.log(current.value)
+    console.log(newTail.value)
+  }
 }
 
 let list = new SinglyLinkedList()
-list.push('hello')
-console.log(list.push('hi'))
+list.push('first')
+list.push('second')
+list.push('third')
+console.log(list.pop())
+console.log(list)
