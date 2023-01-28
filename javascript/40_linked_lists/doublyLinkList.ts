6c789c72e24c7395a098c638166d3e1d42cd2518
AndreFoulem <timmycasual@icloud.com>
2023-01-28 15:58:11 -0800

    Add list node class

--- javascript/40_linked_lists/doublyLinkList.ts
+++ javascript/40_linked_lists/doublyLinkList.ts
@@ -1,1 +1,15 @@
 //? Doubly linked list
+
+type pointer = object | null
+
+class listNode {
+  val: string
+  next: pointer
+  prev: pointer
+
+  constructor(value: string) {
+    this.val = value
+    this.next = null
+    this.prev = null
+  }
+}
