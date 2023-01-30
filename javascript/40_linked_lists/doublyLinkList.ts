566380f0b56dc216fb8818b638d2df0286ecbd3d
AndreFoulem <timmycasual@icloud.com>
2023-01-29 16:15:41 -0800

    Add DoublyLinkList calss

--- javascript/40_linked_lists/doublyLinkList.ts
+++ javascript/40_linked_lists/doublyLinkList.ts
@@ -13,3 +13,17 @@ class listNode {
     this.prev = null
   }
 }
+
+class DoublyLinkedlIst {
+  head: pointer
+  tail: pointer
+  length: number
+
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+}
+
+console.log(
