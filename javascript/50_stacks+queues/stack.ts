4182326063a83f82a96b049e3135e31bcc16d238
AndreFoulem <timmycasual@icloud.com>
2023-02-01 14:53:50 -0800

    Add Stack class

--- javascript/50_stacks+queues/stack.ts
+++ javascript/50_stacks+queues/stack.ts
@@ -1,10 +1,22 @@
 //? stack -> singly linked list
 
-class stackNode {
+class StackNode {
   value: string
   next = null
 
   constructor(value: string) {
     this.value = value
+  }
+}
+
+class Stack {
+  first: object | null
+  last: object | null
+  size: number
+
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
   }
 }
