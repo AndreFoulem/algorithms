bd92de352ab516b1aa139afcb73a0a444075ea93
AndreFoulem <timmycasual@icloud.com>
2023-02-01 15:27:22 -0800

    Add queue + queueNode stack

--- javascript/50_stacks+queues/queues.ts
+++ javascript/50_stacks+queues/queues.ts
@@ -1,1 +1,26 @@
 //? QUEUES
+// First In - First Out
+// USE:
+// -> background tasks
+// -> uploading ressources
+
+class QueueNode {
+  value: string
+  next = null
+
+  constructor(value: string) {
+    this.value = value
+  }
+}
+
+class Queue {
+  first: object | null
+  last: object | null
+  size: number
+
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
+  }
+}
