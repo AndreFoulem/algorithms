9c7984fe68c5f65617d56ba380c553c8097bb31d
AndreFoulem <timmycasual@icloud.com>
2023-02-02 15:34:46 -0800

    Init BST

--- javascript/60_trees/BST.ts
+++ javascript/60_trees/BST.ts
@@ -1,16 +1,20 @@
 class BSTNode {
-  value: string
-  next: null | object = null
+  value: number
+  left: null | object = null
   right: null | object = null
 
-  constructor(value: string) {
+  constructor(value: number) {
     this.value = value
   }
 }
 
-class BST {
-  root: null | string | object
+class BinarySearchTree {
+  root: null | object
   constructor() {
     this.root = null
   }
 }
+
+let tree = new BinarySearchTree()
+tree.root = new BSTNode(10)
+console.log(tree)
