9b16ac6da3e10f786bf58c56ba39ec5fdc4ef0ce
AndreFoulem <timmycasual@icloud.com>
2023-02-02 15:50:36 -0800

    add equality guard

--- javascript/60_trees/BST.ts
+++ javascript/60_trees/BST.ts
@@ -1,3 +1,6 @@
+// @ts-nocheck
+//? Binary Search Tree
+
 class BSTNode {
   value: number
   left: null | object = null
@@ -12,6 +15,33 @@ class BinarySearchTree {
   root: null | object
   constructor() {
     this.root = null
+  }
+
+  insert(value: number) {
+    let newNode = new BSTNode(value)
+    if (!this.root) {
+      this.root = newNode
+      return this
+    }
+    let current = this.root
+    while (true) {
+      if (value === current.value) return 'equal value'
+      if (value < current.value) {
+        if (current.left === null) {
+          //> found a place
+          current.left = newNode
+          return this
+        } else {
+          current = current.left
+        }
+      } else if (value > current.value) {
+        //> found a place
+        if (current.right === null) {
+          current.right = newNode
+          return this
+        } else current = current.right
+      }
+    }
   }
 }
 
