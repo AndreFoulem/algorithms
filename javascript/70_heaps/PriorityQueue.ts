c8001adf933f058355451b4719aa3de0f1457fa9
AndreFoulem <timmycasual@icloud.com>
2023-02-06 12:42:09 -0800

    Add Node class

--- javascript/70_heaps/PriorityQueue.ts
+++ javascript/70_heaps/PriorityQueue.ts
@@ -1,1 +1,79 @@
+// @ts-nocheck
+
 // * -> Priority Queue: w/ Min Heap
+
+class PriorityQueue {
+  constructor() {
+    this.values = []
+  }
+
+  insert(element) {
+    this.values.push(element)
+    this.bubbleup()
+  }
+
+  bubbleup() {
+    //*1 find element index
+    let idx = this.values.length - 1
+    const element = this.values[idx]
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2)
+      let parent = this.values[parentIdx]
+
+      if (element <= parent) break
+      // swap values
+      this.values[parentIdx] = element
+      this.values[idx] = parent
+      idx = parentIdx
+    }
+  }
+
+  extractMax() {
+    const max = this.values[0]
+    const end = this.values.pop()
+    // switch
+    this.values[0] = end
+
+    this.sinkDown()
+    return max
+  }
+  sinkDown() {
+    let idx = 0
+    const length = this.values.length
+    const element = this.values[0]
+    while (true) {
+      // find 2 childs
+      let leftChildIdx = 2 * idx + 1
+      let rightChildIdx = leftChildIdx + 1
+      let leftChild, rightChild
+      let swap = null
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx]
+        if (leftChild > element) {
+          swap = leftChildIdx
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx]
+        if (
+          (swap == null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx
+        }
+      }
+
+      if (swap === null) break
+      this.values[idx] = this.values[swap]
+      this.values[swap] = element
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val
+    this.priority = priority
+  }
+}
