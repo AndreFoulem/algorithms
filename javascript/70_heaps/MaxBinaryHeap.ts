7eefaa64eaf188ba8b82ec98e2864f4ac1dc87d6
AndreFoulem <timmycasual@icloud.com>
2023-02-05 16:34:55 -0800

    Add extract Max method

--- javascript/70_heaps/MaxBinaryHeap.ts
+++ javascript/70_heaps/MaxBinaryHeap.ts
@@ -13,11 +13,20 @@ class MaxBinaryHeap {
     //*1 find element index
     let idx = this.values.length - 1
     const element = this.values[idx]
-    while (true) {
+    while (idx > 0) {
       let parentIdx = Math.floor((idx - 1) / 2)
+      let parent = this.values[parentIdx]
+
+      if (element <= parent) break
+      // swap values
+      this.values[parentIdx] = element
+      this.values[idx] = parent
+      idx = parentIdx
     }
   }
+
+  extractMax() {}
 }
 
-let heap = new MaxBinaryHeap()
+let heap = new MaxBinaryHeap([12, 34, 56, 45, 34, 23])
 heap.insert(55)
