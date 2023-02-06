ca5126575e7d61f6aeaafc53d6cc73a43311efa3
AndreFoulem <timmycasual@icloud.com>
2023-02-06 12:36:32 -0800

    Add priority queue

--- javascript/70_heaps/MaxBinaryHeap.ts
+++ javascript/70_heaps/MaxBinaryHeap.ts
@@ -36,6 +36,35 @@ class MaxBinaryHeap {
   }
   sinkDown() {
     let idx = 0
+    const length = this.values.length
+    const element = this.values[0]
+    while (true) {
+      // find 2 childs
+      let leftChildIdx = 2 * idx + 1
+      let rightChildIdx = leftChildIdx + 1
+      let leftChild, rightChild
+      let swap = null
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx]
+        if (leftChild > element) {
+          swap = leftChildIdx
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx]
+        if (
+          (swap == null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx
+        }
+      }
+
+      if (swap === null) break
+      this.values[idx] = this.values[swap]
+      this.values[swap] = element
+    }
   }
 }
 
---
+++ javascript/70_heaps/PriorityQueue.ts
@@ -0,0 +1,1 @@
+// * -> Priority Queue: w/ Min Heap
