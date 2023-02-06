87d36a2aa86c33351cbeab4d06eba84686550f29
AndreFoulem <timmycasual@icloud.com>
2023-02-05 16:21:13 -0800

    Add bubbleup helper method

--- javascript/70_heaps/MaxBinaryHeap.ts
+++ javascript/70_heaps/MaxBinaryHeap.ts
@@ -3,4 +3,21 @@ class MaxBinaryHeap {
   constructor(values: number[]) {
     this.values = values
   }
+
+  insert(element: number) {
+    this.values.push(element)
+    this.bubbleup()
+  }
+
+  bubbleup() {
+    //*1 find element index
+    let idx = this.values.length - 1
+    const element = this.values[idx]
+    while (true) {
+      let parentIdx = Math.floor((idx - 1) / 2)
+    }
+  }
 }
+
+let heap = new MaxBinaryHeap()
+heap.insert(55)
