a3204b3dd092a0e47cd1dbae5844d7fb98bcce3e
AndreFoulem <timmycasual@icloud.com>
2023-02-09 12:42:47 -0800

    Update hash fn

--- javascript/80_hash_tables/hash.ts
+++ javascript/80_hash_tables/hash.ts
@@ -2,12 +2,12 @@ console.log('A'.charCodeAt(0))
 
 function hash(key: string, arrayLen: number): number {
   let total = 0
-  let WEIRD_PRIME = 1
+  let SHOTGUN_PRIME = 31
 
   for (let i = 0; i < Math.min(key.length, 100)) {
    let char = key[i]
     let value = char.charCodeAt(0) - 96
-    total = (total + WEIRD_PRIME + value) % arrayLen
+    total = (total + SHOTGUN_PRIME + value) % arrayLen
   }
 
   return total
