02b11402c5f429254f83f9446082ad0c7f56c4a2
AndreFoulem <timmycasual@icloud.com>
2023-02-10 17:08:00 -0800

    Remove edge fn

--- javascript/90_graphs/UndirectedGraph_AgencyList.ts
+++ javascript/90_graphs/UndirectedGraph_AgencyList.ts
@@ -8,4 +8,27 @@ class UndirectedGraph_AgencyList {
   addVertex(vertex) {
     this.adjacencyList[vertex] = []
   }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2)
+    this.adjacencyList[vertex2].push(vertex1)
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    )
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    )
+  }
 }
+
+let graph = new UndirectedGraph_AgencyList()
+graph.addVertex('San Francisco')
+graph.addVertex('Turin')
+graph.addVertex('New York')
+graph.addEdge('San Francisco', 'Turin')
+graph.removeEdge('San Francisco', 'Turin')
+
+console.log(graph)
