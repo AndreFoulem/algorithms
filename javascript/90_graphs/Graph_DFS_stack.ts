990e14389ce2a639a4dd87f2fa9ffb74275e0a71
AndreFoulem <timmycasual@icloud.com>
2023-02-11 13:54:41 -0800

    Add type

---
+++ javascript/90_graphs/Graph_DFS_stack.ts
@@ -0,0 +1,21 @@
+let graphAdjacencyList: {
+  a: string[]
+  b: string[]
+  c: string[]
+  d: string[]
+  e: string[]
+  f: string[]
+}
+
+const depthFirstPrint = (graph, source) => {
+  const stack = []
+}
+
+graphAdjacencyList = {
+  a: ['a', 'c'],
+  b: ['d'],
+  c: ['e'],
+  d: ['f'],
+  e: [],
+  f: [],
+}
