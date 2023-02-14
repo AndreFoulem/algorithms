420cb33a9ad8363c2b61e448f2446832330c8389
AndreFoulem <timmycasual@icloud.com>
2023-02-14 11:53:26 -0800

    Implement acyclyc

--- javascript/90_graphs/_hasPath_BFS.ts
+++ javascript/90_graphs/_hasPath_BFS.ts
@@ -1,4 +1,4 @@
-//* Has Path?
+//* Has Path? w/ acyclic
 //* -> Problem: Can we move to source F to destination K node.
 
 //? Iterative -> Stack
@@ -13,3 +13,21 @@
  *   to
  *   (K)
  */
+
+//& - ENTITY: GRAPH | SOURCE | DESTINATION | NEIGHBOURS
+
+const hasPath = (graph, src, dst) => {
+  //* Source is my guard base case
+  if (src === dst) return true
+
+  for (let neighbor of graph[src]) {
+    // ex:1  let F of array
+    // call recursively
+    if (hasPath(graph, neighbor, dst) == true) {
+      return true
+    }
+  }
+
+  //* return false if not winning path
+  return false
+}
