8bc3dbe188fb2a3437e310af0cf2747050157ad8
AndreFoulem <timmycasual@icloud.com>
2023-02-14 12:25:40 -0800

    Add Adjancy List graph helper builder

--- javascript/90_graphs/_undirectedPath.ts
+++ javascript/90_graphs/_undirectedPath.ts
@@ -1,12 +1,12 @@
-const graphUndirected = {
-  i: ['j', 'k'],
-  j: ['i'],
-  k: ['i', 'm', 'l'],
-  m: ['k'],
-  l: ['l'],
-  o: ['n'],
-  n: ['o'],
-}
+// const graphUndirected = {
+//   i: ['j', 'k'],
+//   j: ['i'],
+//   k: ['i', 'm', 'l'],
+//   m: ['k'],
+//   l: ['l'],
+//   o: ['n'],
+//   n: ['o'],
+// }
 
 /**
  *   i - j
@@ -22,3 +22,33 @@ const graphUndirected = {
 //* src: i    dist: l
 
 //* 1 -> modify edges obj to Adjancy list
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n'],
+]
+
+const buildGraph = (edges) => {
+  const graph = {}
+
+  for (let edge of edges) {
+    const [a, b] = edge
+
+    if (!(a in graph)) graph[a] = []
+    if (!(b in graph)) graph[b] = []
+
+    graph[a].push(b)
+    graph[b].push(a)
+  }
+
+  return graph
+}
+
+//* 2 Implement a depth first traversal
+const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges)
+}
+
+undirectedPath(edges, 'i', 'l')
