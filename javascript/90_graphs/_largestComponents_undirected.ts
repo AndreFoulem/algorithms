73a22038ca2233d2c4d1c3a7b0e6d9c1327c8636
AndreFoulem <timmycasual@icloud.com>
2023-02-16 12:39:01 -0800

    Add components

--- javascript/90_graphs/_largestComponents_undirected.ts
+++ javascript/90_graphs/_largestComponents_undirected.ts
@@ -1,1 +1,12 @@
 //* largest components
+
+//! ENTITY: LARGEST | COUNT | NODE | VISITED
+
+const largestComponent = (graph) => {
+  let longest = 0
+  for (let node in graph) {
+    const size = exploreSize(graph, node)
+    if (size > longest) longest = size
+  }
+  return longest
+}
