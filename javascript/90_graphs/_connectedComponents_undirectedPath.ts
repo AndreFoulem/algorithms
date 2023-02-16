d7e65dbe79c3c352e0cc7a1a7340ef22c175b8d4
AndreFoulem <timmycasual@icloud.com>
2023-02-15 16:00:07 -0800

    Add explore fn

--- javascript/90_graphs/_connectedComponents_undirectedPath.ts
+++ javascript/90_graphs/_connectedComponents_undirectedPath.ts
@@ -1,1 +1,23 @@
-//* -> How many undirected connected components
+//* -> How many undirected connected components (graph region)
+
+//* ENTITY:  Count | Nodes | Visited
+
+const connectedComponentCount = (graph) => {
+  const visited = new Set()
+
+  for (let node in graph) {
+    explore(graph, node, visited)
+  }
+}
+
+const explore = (graph, current, visited) => {
+  if (visited.has(current)) return false
+
+  visited.add(current)
+
+  for (let neighbor of graph[current]) {
+    explore(graph, neighbor, visited)
+  }
+
+  return true
+}
