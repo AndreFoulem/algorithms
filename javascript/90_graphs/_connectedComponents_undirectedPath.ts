4636c6bbfa850775f1a8c42933ec03fa8b84bdb4
AndreFoulem <timmycasual@icloud.com>
2023-02-15 16:03:15 -0800

    Cast to string

--- javascript/90_graphs/_connectedComponents_undirectedPath.ts
+++ javascript/90_graphs/_connectedComponents_undirectedPath.ts
@@ -4,16 +4,21 @@
 
 const connectedComponentCount = (graph) => {
   const visited = new Set()
+  let count = 0
 
   for (let node in graph) {
-    explore(graph, node, visited)
+    if (explore(graph, node, visited) == true) {
+      count += 1
+    }
   }
+
+  return count
 }
 
 const explore = (graph, current, visited) => {
-  if (visited.has(current)) return false
+  if (visited.has(String(current))) return false
 
-  visited.add(current)
+  visited.add(String(current))
 
   for (let neighbor of graph[current]) {
     explore(graph, neighbor, visited)
