379722910b2599171ff60a4d823b7f826211b94e
AndreFoulem <timmycasual@icloud.com>
2023-02-17 13:50:21 -0800

    Add islandCount

--- javascript/90_graphs/_islandCount.ts
+++ javascript/90_graphs/_islandCount.ts
@@ -2,8 +2,21 @@
 
 // * Looking for neighbors - removing 1 from rows or columns
 
+//* TIME: 0(rc)
+//* SPACE: 0(rc)
+
 /**
  *               (r-1, c)
  *   (r, c-1)    [r,c]     (r, c+1)
  *               (r+1,c)
  */
+
+//? ENTITY: VISITED | NEIGHBORS | COLS | ROWS | ISLAND_COUNT
+
+const islandCount = (grid) => {
+  for (let r = 0; r < grid.length; r += 1) {
+    for (let c = 0; c < grid[0].length; c += 1) {
+      explore(grid, r, c)
+    }
+  }
+}
