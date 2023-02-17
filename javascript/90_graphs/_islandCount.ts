79298c46b2e409d75dd0b816f45167b5e3a068fb
AndreFoulem <timmycasual@icloud.com>
2023-02-17 14:00:55 -0800

    Add explore helper

--- javascript/90_graphs/_islandCount.ts
+++ javascript/90_graphs/_islandCount.ts
@@ -1,3 +1,4 @@
+export {}
 //* Grid graph (row, col)
 
 // * Looking for neighbors - removing 1 from rows or columns
@@ -12,11 +13,35 @@
  */
 
 //? ENTITY: VISITED | NEIGHBORS | COLS | ROWS | ISLAND_COUNT
+//! position == node
 
 const islandCount = (grid) => {
+  const visited = new Set()
+
   for (let r = 0; r < grid.length; r += 1) {
     for (let c = 0; c < grid[0].length; c += 1) {
-      explore(grid, r, c)
+      explore(grid, r, c, visited)
     }
   }
+}
+
+const explore = (grid, r, c, visited) => {
+  //
+  const rowInbounds = 0 <= r && r < grid.length
+  const colInbounds = 0 <= r && r < grid.length
+  if (!rowInbounds || !colInbounds) return false
+
+  if (grid[r][c] === 'w') return false
+  const pos = r + ',' + c
+
+  if (visited.has(pos)) return false
+  visited.add(pos)
+
+  //-> I'm here at unvisited land
+  explore(grid, r - 1, c, visited)
+  explore(grid, r + 1, c, visited)
+  explore(grid, r, c - 1, visited)
+  explore(grid, r, c + 1, visited)
+
+  return true
 }
