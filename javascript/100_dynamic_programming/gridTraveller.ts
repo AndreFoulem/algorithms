7eaedb46095572721047cb4aa7d046060cab8e54
AndreFoulem <timmycasual@icloud.com>
2023-02-20 11:15:15 -0800

    Add base cases

--- javascript/100_dynamic_programming/gridTraveller.ts
+++ javascript/100_dynamic_programming/gridTraveller.ts
@@ -1,1 +1,6 @@
-//* Grid traveler -> Tuple (x,y)
+//* Grid traveler -> Dimension: Tuple (x,y)
+
+const gridTraveler = (m, n) => {
+  trip: if (m === 1 && n === 1) return 1
+  invalid: if (m === 0 || n === 0) return 0
+}
