192910d31d10d76c31dea35e0a819aaaf17af092
AndreFoulem <timmycasual@icloud.com>
2023-02-20 11:17:25 -0800

    Implement without memo

--- javascript/100_dynamic_programming/gridTraveller.ts
+++ javascript/100_dynamic_programming/gridTraveller.ts
@@ -3,4 +3,10 @@
 const gridTraveler = (m, n) => {
   trip: if (m === 1 && n === 1) return 1
   invalid: if (m === 0 || n === 0) return 0
+
+  //-> return [ down ] + [ right ]
+  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
 }
+
+console.log(gridTraveler(1, 1))
+console.log(gridTraveler(3, 3))
