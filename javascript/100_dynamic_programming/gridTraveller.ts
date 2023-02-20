9043f9c13764521a89b0675869cfde9aa2b6e33a
AndreFoulem <timmycasual@icloud.com>
2023-02-20 11:27:15 -0800

    Add memoization

--- javascript/100_dynamic_programming/gridTraveller.ts
+++ javascript/100_dynamic_programming/gridTraveller.ts
@@ -1,12 +1,27 @@
 //* Grid traveler -> Dimension: Tuple (x,y)
 
-const gridTraveler = (m, n) => {
+const gridTraveler = (m, n, memo = {}) => {
+  const key = m + ',' + n
+  if (key in memo) return memo[key]
+
   trip: if (m === 1 && n === 1) return 1
   invalid: if (m === 0 || n === 0) return 0
 
-  //-> return [ down ] + [ right ]
-  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+  // -> return [ down ] + [ right ]
+  memo[key] = gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+  return memo[key]
 }
 
 console.log(gridTraveler(1, 1))
 console.log(gridTraveler(3, 3))
+
+/**
+ * Complexity - Brute Force - (binary tree)
+ * HEIGHT: (2,3) -> 4
+ * TIME: O(2n+m)
+ * SPACE: P(n+M)
+ */
+
+/**
+ * Complexity - Memo - ** grid(a,b) == grid(b,a)
+ */
