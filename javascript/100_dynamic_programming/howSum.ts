e13c39b49ce881137c515cb4f68a20cc08f250e5
AndreFoulem <timmycasual@icloud.com>
2023-02-21 10:45:45 -0800

    Add memo

--- javascript/100_dynamic_programming/howSum.ts
+++ javascript/100_dynamic_programming/howSum.ts
@@ -1,17 +1,20 @@
 //* Write 'howSum(targetSum, [numbers]) -> returns array with a first combination of elements that sums to targetSum || return null
 //**  Can reuse elements for sum */
 
-const howSum = (targetSum, numbers) => {
+const howSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum]
   if (targetSum === 0) return []
   if (targetSum < 0) return null
 
   for (let num of numbers) {
     const remainder = targetSum - num
-    const remainderResult = howSum(remainder, numbers)
+    const remainderResult = howSum(remainder, numbers, memo)
     if (remainderResult !== null) {
-      return [...remainderResult, num]
+      memo[targetSum] = [...remainderResult, num]
+      return memo[targetSum]
     }
   }
+  memo[targetSum] = null
   return null
 }
 
@@ -29,6 +32,6 @@ console.log(howSum(8, [2, 3, 5]))
  * MEMO COMPLEXITY
  *  m = target sum
  *  n= numbers.length
- *  TIME: O(n^m * m)
- *  SPACE: O(m)
+ *  TIME: O(n*m^m)
+ *  SPACE: O(m*m)
  */
