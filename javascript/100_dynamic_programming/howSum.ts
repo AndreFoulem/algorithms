8953708200f67b7f424a613142bcfd788b9803f7
AndreFoulem <timmycasual@icloud.com>
2023-02-21 10:35:33 -0800

    Add recursive howSum

--- javascript/100_dynamic_programming/howSum.ts
+++ javascript/100_dynamic_programming/howSum.ts
@@ -4,4 +4,15 @@
 const howSum = (targetSum, numbers) => {
   if (targetSum === 0) return []
   if (targetSum < 0) return null
+
+  for (let num of numbers) {
+    const remainder = targetSum - num
+    const remainderResult = howSum(remainder, numbers)
+    if (remainderResult !== null) {
+      return [...remainderResult, num]
+    }
+  }
+  return null
 }
+
+console.log(howSum(8, [2, 3, 5]))
