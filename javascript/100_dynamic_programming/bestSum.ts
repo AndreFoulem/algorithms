45232be8a3fb2007ab7c7fe7f8607894e347087a
AndreFoulem <timmycasual@icloud.com>
2023-02-22 15:16:37 -0800

    Implement loops

--- javascript/100_dynamic_programming/bestSum.ts
+++ javascript/100_dynamic_programming/bestSum.ts
@@ -3,3 +3,30 @@
 /**
  * Write bestSum(targetSum: int, [numbers: int])  -> shortest combination for sum
  */
+
+const bestSum = (targetSum, numbers) => {
+  if (targetSum === 0) return []
+  if (targetSum < 0) return null
+
+  let shortestCombination = null
+
+  for (let num in numbers) {
+    const remainder = targetSum - Number(num)
+    const remainderCombination = bestSum(remainder, numbers)
+
+    if (remainderCombination !== null) {
+      const combination = [...remainderCombination, num]
+
+      if (
+        shortestCombination === null ||
+        combination.length < shortestCombination.length
+      ) {
+        shortestCombination = combination
+      }
+    }
+  }
+
+  return shortestCombination
+}
+
+console.log(bestSum(7, [5, 3, 4, 7]))
