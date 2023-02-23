bc810795315205e11c9cb17c3b14afc2f8fb6c1f
AndreFoulem <timmycasual@icloud.com>
2023-02-23 11:15:40 -0800

    Add canConstruct fn

--- javascript/100_dynamic_programming/canConstruct.ts
+++ javascript/100_dynamic_programming/canConstruct.ts
@@ -1,1 +1,14 @@
 // -> Can construct
+
+//*  Write canConstruct(target: String, [wordBank]: String) -> Bool
+//* Returns true if target can be constructed by concatenating of arr elements. ** May reuse elements
+
+//& ex: canConstruct(abcdef, [ab, abc, cd, def, abcd]) -> true
+
+//& ex: canConstruct('', [ cat, dog, mouse ] ) -> true. Takes place in all place... base case?
+
+const canConstruct = (target, wordBank) => {
+  if (target === '') return true
+}
+
+console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))
