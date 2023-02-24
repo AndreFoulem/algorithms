7c843d526cb2877d909ea00bfd544e188d09db37
AndreFoulem <timmycasual@icloud.com>
2023-02-24 15:46:47 -0800

    Add iteration

--- javascript/100_dynamic_programming/101_dyn_prog_TABULATION/fibonnaci_tab.ts
+++ javascript/100_dynamic_programming/101_dyn_prog_TABULATION/fibonnaci_tab.ts
@@ -2,6 +2,12 @@
 
 const fibo = (n) => {
   const table = Array(n + 1).fill(0)
+  table[1] = 1
+  for (let i = 0; i <= n; i++) {
+    table[i + 1] += table[i]
+    table[i + 2] += table[i]
+  }
+  return table[n]
 
   console.log(table)
 }
