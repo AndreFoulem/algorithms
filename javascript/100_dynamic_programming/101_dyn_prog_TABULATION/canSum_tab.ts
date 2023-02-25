d2aadf12e4e753d0f428e4bf173330d6ee177fc3
AndreFoulem <timmycasual@icloud.com>
2023-02-25 15:25:17 -0800

    Add for loops

--- javascript/100_dynamic_programming/101_dyn_prog_TABULATION/canSum_tab.ts
+++ javascript/100_dynamic_programming/101_dyn_prog_TABULATION/canSum_tab.ts
@@ -14,6 +14,18 @@ export {}
 const canSum = (targetSum, numbers) => {
   //-> init an arr with bool(false)
   const table = Array(targetSum + 1).fill(false)
+
+  table[0] = true
+
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i] === true) {
+      for (let num of numbers) {
+        table[i + num] = true
+      }
+    }
+  }
+
+  return table[targetSum]
 }
 
 console.log(canSum(7, [5, 4, 3]))
