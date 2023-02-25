ed29415d216368cb23abba52a3efc722fe7d2afd
AndreFoulem <timmycasual@icloud.com>
2023-02-25 15:04:00 -0800

    Add docs

--- javascript/100_dynamic_programming/101_dyn_prog_TABULATION/canSum_tab.ts
+++ javascript/100_dynamic_programming/101_dyn_prog_TABULATION/canSum_tab.ts
@@ -1,3 +1,19 @@
+export {}
 //-> Can sum
 
 //-* Write canSum(targetSum: Int, numbers: [Int]) -> bool
+
+//-** Can reuse elements
+
+//-ex: canSum(7, [5,4,3]) -> true
+
+//-params: m = targetSum  n = numbers.length
+
+//-time: O(mn)  space: 0(m)
+
+const canSum = (targetSum, numbers) => {
+  //-> init an arr with bool(false)
+  const table = Array(targetSum + 1).fill(false)
+}
+
+console.log(canSum(7, [5, 4, 3]))
